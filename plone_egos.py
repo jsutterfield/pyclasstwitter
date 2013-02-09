5a5040153fdf489c7ad84eabea1d461dabba7efd
Kellan Jacobs <kellankade@gmail.com>
2013-02-09 14:50:21 -0800

    Added command line parsing with argparse

--- plone_egos.py
+++ plone_egos.py
@@ -1,17 +1,36 @@
-def send_hashtag_report(hashtag, *email_to):
+import argparse
+
+
+def send_hashtag_report(hashtag, email_to):
     pass
+
 
 def get_tweets(hashtag):
     pass
 
+
 def get_image(image_url):
     pass
 
+
 def prepare_email(tweets):
-  pass
+    pass
 
-def send_email(content, addresses, host, port, to, from, subject, images):
-  pass
 
-if __name__ == 'main':
-    send_hashtag_report()
+def send_email(content, addresses, host, port, to, from_address, subject,
+               images):
+    pass
+
+if __name__ == '__main__':
+
+    # Create Argparser
+    parser = argparse.ArgumentParser(description="Sends and email report for \
+                                    a specific hashtag")
+    parser.add_argument("HASHTAG", help="The hashtag you want to run the \
+                        report on")
+    parser.add_argument('EMAIL', nargs='*', help="List of email addresses to \
+                        #send too")
+
+    cmdargs = parser.parse_args()
+
+    send_hashtag_report(cmdargs.HASHTAG, cmdargs.EMAIL)
