32c9f1fcf90193f81be7c4b0d85fc345e42b7925
James Sutterfield <james.sutterfield@gmail.com>
2013-02-09 13:46:54 -0800

    Functions signatures

---
+++ plone_egos.py
@@ -0,0 +1,17 @@
+def send_hashtag_report(hashtag, *email_to):
+    pass
+
+def get_tweets(hashtag):
+    pass
+
+def get_image(image_url):
+    pass
+
+def prepare_email(tweets):
+  pass
+
+def send_email(content, addresses, host, port, to, from, subject, images):
+  pass
+
+if __name__ == 'main':
+    send_hashtag_report()
